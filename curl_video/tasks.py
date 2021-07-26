6487abea7f8c5da5a8e67a38ae53182a563330e3
soudabemhashemi <s.mhashemi.221@gmail.com>
2021-07-26 12:26:46 +0430

    .

--- curl_video/tasks.py
+++ curl_video/tasks.py
@@ -26,18 +26,3 @@ def crawl_aparat():
 
 
 
-# @shared_task
-# def summary_in_last_day():
-#     t = datetime.now()
-#     last_day = t.replace(day=int(t.strftime("%d"))-1)
-#     ad_list = Ad.objects.all()
-#     for ad in ad_list:
-#         NOview = summaryShit.objects.filter(adID=ad, date__range=(last_day, datetime.now()), view_or_click=1).count()
-#         new_obj = summaryShit(adID=ad, date=last_day, count=NOview, view_or_click=1)
-#         new_obj.save()
-#         NOclick = summaryShit.objects.filter(adID=ad, date__range=(last_day, datetime.now()), view_or_click=0).count()
-#         new_obj = summaryShit(adID=ad, date=last_day, count=NOclick, view_or_click=0)
-#         new_obj.save()
-
-
-
